c120ae62515c749092589750cd8cb68b4155f69a
esnahow <shouhui.wang@ericsson.com>
2019-02-11 17:22:28 +0800

    a

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+
+/**
+ * @author esnahow
+ * @date 02/11/2019 17:09
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello World");
+	}
+
+}
